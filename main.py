575ecacf904e2d6a9311910f89c77b916bc10856
Остап Грушецький <grushevskiy_o@icloud.com>
2023-08-10 23:59:57 +0300

    Fix all issues

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ def greeting(name):
     return greet
 
 
+def test_func():
+    return None
+
+
 print(greeting("Ostap"))
+<<<<<<< HEAD
 
 
 def bug_fix():
     print("top")
     
+=======
+print(test_func())
+>>>>>>> feat1
